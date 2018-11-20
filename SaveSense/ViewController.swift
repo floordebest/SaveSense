6cd8f39515e7ff05d1e400af89bc4174fce66e00
Floor de Best <floor@macbook-pro-van-floor.local>
2018-11-20 17:54:20 +0100

    Initial Commit

---
+++ SaveSense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveSense
+//
+//  Created by Floor de Best on 20/11/2018.
+//  Copyright © 2018 dBest Innovations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SaveSenseTests/SaveSenseTests.swift
@@ -0,0 +1,34 @@
+//
+//  SaveSenseTests.swift
+//  SaveSenseTests
+//
+//  Created by Floor de Best on 20/11/2018.
+//  Copyright © 2018 dBest Innovations. All rights reserved.
+//
+
+import XCTest
+@testable import SaveSense
+
+class SaveSenseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
